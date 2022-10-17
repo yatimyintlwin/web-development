45a6e1ac317b9c92e05ea66928c5c6e95bbead0b
Yati Myint Lwin <yatimyintlwin43@gmail.com>
2022-10-17 21:21:40 +0630

    JavaScript Exercises Added

--- JavaScript/exercise1.js
+++ JavaScript/exercise1.js
@@ -1,5 +1,7 @@
-var num1 = 3;
-var num2 = 5;
+{
+  var num1 = 3;
+  let num2 = 5;
+}
 var num3 = num1 + num2;
 console.log("num1 is : " + num1);
 console.log("num2 is : " + num2);
---
+++ JavaScript/exercise3.js
@@ -0,0 +1,9 @@
+let a,
+  b,
+  c = -3;
+const PI = -3.14;
+a = 5;
+console.log("a is : " + a);
+console.log("b is : " + b);
+console.log("c is : " + c);
+console.log("PI is : " + PI);
