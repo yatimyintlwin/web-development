cbe4779716cce7318668b54cd1de0aa079287369
Yati Myint Lwin <yatimyintlwin43@gmail.com>
2022-10-19 21:45:02 +0630

    JavaScript Exercises Added

---
+++ JavaScript/exercise4.js
@@ -0,0 +1,31 @@
+let a = 30;
+let b = 12;
+let c = 3;
+let d = (a * b) / c;
+console.log("d is : " + d);
+
+let e = "Hello ";
+let f = "World ";
+let g = "Hey";
+let h = e + f + g;
+console.log("h is : " + h);
+
+let i = true;
+let j = false;
+console.log(i + ", " + j);
+
+let k = "I";
+let l = "have";
+let m = 30;
+let n = "Kyats";
+console.log(`${k} ${l} ${m} ${n}`);
+
+console.log("ABC" > 123);
+
+console.log("You need 30' distance.");
+console.log("I'm not \na robot.");
+console.log('Where\'s my "Apple"?');
+
+let name = "Bob";
+let greet = `Hello ${name}`;
+console.log("greet is : " + greet);
