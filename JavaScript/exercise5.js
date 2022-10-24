02ec5cd17c03dd728326db924a0b2bc8b9f61c50
Yati Myint Lwin <yatimyintlwin43@gmail.com>
2022-10-24 20:46:38 +0630

    JavaScript Exercise Added

---
+++ JavaScript/exercise5.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+  return a + b;
+}
+
+function circle(r) {
+  const PI = 3.14;
+  return PI * r * r;
+}
+
+function greet() {
+  console.log("Hello, World!");
+}
+
+function sayHello(name) {
+  console.log(`Hello ${name}`);
+}
+
+console.log(add(7, 8));
+console.log(circle(3));
+greet();
+sayHello("Alice");
