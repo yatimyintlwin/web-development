57b2c3b3a18e92f98d4c0ef35df35758112ae1bf
Yati Myint Lwin <yatimyintlwin43@gmail.com>
2022-10-25 21:41:05 +0630

    JavaScript Exercise Added

---
+++ JavaScript/exercise6.js
@@ -0,0 +1,14 @@
+function welcome(name) {
+  console.log(`Welcome, ${name}`);
+}
+welcome("Alex");
+
+function num(a, b, c) {
+  return a * b * c;
+}
+console.log(num(4, 5, 6));
+
+function isOdd(a) {
+  return a % 2 !== 0;
+}
+console.log(isOdd(13));
