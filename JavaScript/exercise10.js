e223daf1e8a55a69c09af67ff3a3a13ce274f6f6
Yati Myint Lwin <yatimyintlwin43@gmail.com>
2022-11-28 21:36:58 +0630

    JavaScript Exercises Added

--- JavaScript/exercise10.js
+++ JavaScript/exercise10.js
@@ -23,3 +23,34 @@ console.log(results);
 
 let results2 = [...nums2, ...alphas];
 console.log(results2);
+
+let four = [...nums2, 4];
+console.log(four);
+
+let zero = [0, ...nums2];
+console.log(zero);
+
+function add(a, b) {
+  return a + b;
+}
+
+let nums3 = [123, 456];
+console.log(add(nums3[0], nums3[1]));
+
+console.log(add(...nums3));
+
+let a = nums3[0];
+console.log(a);
+
+let b = nums3[1];
+console.log(b);
+
+let [c, d] = nums3;
+console.log(c);
+console.log(d);
+
+function add2([a, b]) {
+  return a + b;
+}
+
+console.log(add2(nums3));
---
+++ JavaScript/exercise11.js
@@ -0,0 +1,26 @@
+let num = 3.14159;
+console.log(num.toFixed(3));
+
+let name = "Bob";
+let greet = `Hello ${name}`;
+let welcome = new String("Welcome");
+
+console.log(name.length);
+console.log(welcome.length);
+console.log("Hello".length);
+
+let name2 = "Alice";
+
+console.log(name2[2]);
+console.log(name2.charAt(0));
+
+console.log(name2.toUpperCase());
+console.log(name2.toLowerCase());
+console.log(name2.substring(0, 3));
+
+let text = "  Hello World  ";
+console.log(text.trim());
+
+let texts = "Hello World";
+console.log(texts.split(" "));
+console.log(texts.split(""));
